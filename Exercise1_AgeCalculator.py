7ef78a1768f77d770c0b6e7ee4b77085431cb93f
Samztitha S <samztithasamztitha@gmail.com>
2025-08-23 11:47:24 +0530

    Update Exercise1_AgeCalculator.py

--- Exercise1_AgeCalculator.py
+++ Exercise1_AgeCalculator.py
@@ -2,20 +2,21 @@ from datetime import datetime
 
 try:
     # Ask for birthdate in mm/dd/yyyy format
-    birth_input = input("Enter your birth date (mm/dd/yyyy): ")
+    b_input = input("Enter your birth date (mm/dd/yyyy): ")
     
     # Validate and convert to datetime
-    birth_date = datetime.strptime(birth_input, "%m/%d/%Y")
+    b_date = datetime.strptime(b_input, "%m/%d/%Y")
     
     # Get today’s date
     today = datetime.today()
     
     # Calculate age
-    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    age = today.year - b_date.year - ((today.month, today.day) < (b_date.month, b_date.day))
     print(f" Your current age is: {age} years")
     
     # Convert and display in IST format (dd/mm/yyyy)
-    print(" Your birthdate in IST format:", birth_date.strftime("%d/%m/%Y"))
+    print(" Your birthdate in IST format:", b_date.strftime("%d/%m/%Y"))
 
 except ValueError:
     print("Invalid date format! Please enter in mm/dd/yyyy format.")
+
